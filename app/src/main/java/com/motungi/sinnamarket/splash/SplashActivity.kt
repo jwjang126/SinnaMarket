0a791632a5718e01f1dd5358e5445c62d30883c8
jwjang126 <jwjang126@gmail.com>
2025-08-13 14:03:43 +0900

    auto login

--- app/src/main/java/com/motungi/sinnamarket/splash/SplashActivity.kt
+++ app/src/main/java/com/motungi/sinnamarket/splash/SplashActivity.kt
@@ -3,17 +3,27 @@ package com.motungi.sinnamarket.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.motungi.sinnamarket.R
 import com.motungi.sinnamarket.auth.LoginActivity
+import com.motungi.sinnamarket.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+        val auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (currentUser != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
             finish()
         }, 2000) // 2초 후 로그인 화면으로
     }
